1b5d92c824fdcd46d77fd89bf827c66d0eaae604
Nikita Shirikov <nikitashirikov@MacBook-Pro-Nikita.local>
2022-02-17 16:43:43 +0300

    Initial commit

---
+++ src/main/java/com/example/bootproject/MyController.java
@@ -0,0 +1,35 @@
+package com.example.bootproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private  UserReopository ur;
+
+    @GetMapping(path = "/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping(path = "/add")
+    public @ResponseBody String addNewUser(@RequestParam String name,@RequestParam String email){
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        ur.save(user);
+        return "Saved!";
+
+    }
+
+    @GetMapping("/all")
+    public @ResponseBody Iterable<User> getAllUsers(){
+        return ur.findAll();
+    }
+}
